65ffcd59a8a01beae9c8f2c5e96c3b59ff5ce3ff
AbihaFatma <abihafatma12@gmail.com>
2024-08-12 00:55:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const ticketRoutes = require("./routes");
+
+const connectDB = require("./dbConnection");
+const Ticket = require("./schema");
+
+app.use(cors());
+app.use(express.json());
+connectDB();
+app.use("/api", ticketRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`app is listening ${PORT}`);
+});
